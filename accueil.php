b1b9e51a241bae74dead76b20e86af3419a930a6
Firstname LastnameC:wampwww1BTSbijouterieaccueil.php <faustine.renard@gmail.com>
2012-04-29 23:16:04 +0200

    first commit

---
+++ accueil.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title>Bijouterie plouay</title>
+<link rel="stylesheet" type="text/css" href="feuilleStyle3.css"/>
+</head>
+<body>
+<div id="container">
+	<div id='header'><img src="Montage.bmp"></div>
+	<div id='menu'>
+		<br><br>
+		<a href ="accueil.php">Accueil</a><br><br>
+		<a href ="alliance.php">Les alliances</a></br><br>
+		<a href ="collier.php">Les colliers</a></br><br>
+		<a href ="bague.php">Les bagues</a></br><br>
+		<a href ="Bl.php">Les boucles d'oreilles</a></br><br>
+		<a href ="Br.php">Les bracelets</a></br><br>
+		<a href ="Montre.php">Les montres</a></br><br>
+		<a href ="Modification.php">Modification</a><br><br>
+		<a href ="recherche.php">Recherche</a><br><br>
+	</div>
+	<div id="centre"><h3>Présentation de l'entreprise</h3>
+		<br><br><br>
+L'ouverture de la bijouterie s'est faite en mai 2001 par Nathalie Toulgoat. Elle est spécialisée dans la vente d'alliance.<br>
+La bijouterie vous propose également des réparations sur place de bijoux par Guillaume, ouvrier à l'atelier bijouterie,  
+et horlogerie par Damien, ouvrier à l'atelier horlogerie.<br>
+Vous serez accueillit chaleureusement par la dirigeante, Nathalie Toulgoat, qui vous conseillera dans vos choix.<br>
+Vous trouverez également une deuxième bijouterie, ouverte depuis novembre 2009, au Faoüet. 
+	</div> 
+<div id='content'></div>
+</div>
+</body>
+</html>
